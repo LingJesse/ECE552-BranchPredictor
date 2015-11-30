56b3c53294f2df8b5fd34bfc7607dbb29f3057f5
Bitnami <bitnami@ubuntu14-generic-template-01.oit.duke.edu>
2015-11-29 22:40:43 -0500

    Setting up for path based prediction implementation

--- pin-2.14-71313-gcc.4.4.7-linux/source/tools/ManualExamples/itrace.cpp
+++ pin-2.14-71313-gcc.4.4.7-linux/source/tools/ManualExamples/itrace.cpp
@@ -1,52 +1,116 @@
-/*BEGIN_LEGAL 
-Intel Open Source License 
-
-Copyright (c) 2002-2015 Intel Corporation. All rights reserved.
- 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.  Redistributions
-in binary form must reproduce the above copyright notice, this list of
-conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.  Neither the name of
-the Intel Corporation nor the names of its contributors may be used to
-endorse or promote products derived from this software without
-specific prior written permission.
- 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE INTEL OR
-ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-END_LEGAL */
 #include <stdio.h>
+#include <math.h>
 #include "pin.H"
 
+const int BHT_BITS = 8; // set the number of bits to use for the BHT (1 disables this)
+const int PATH_DEPTH = 16; // set the number of addresses to use in history aka correlation depth (1 disables this)
+const int TABLE_ENTRIES = 1<<PATH_DEPTH;
+
 FILE * trace;
+int bhr[PATH_DEPTH];
+int bhr_current_index;
+string branch_address_history[PATH_DEPTH]; // stores the current recent history of branch addresses
+bool prediction;
+int num_branches;
+int num_correct;
 
-// This function is called before every instruction is executed
-// and prints the IP
-VOID printip(VOID *ip) { fprintf(trace, "%p\n", ip); }
-
-// Pin calls this function every time a new instruction is encountered
-VOID Instruction(INS ins, VOID *v)
-{
-    // Insert a call to printip before every instruction, and pass it the IP
-    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)printip, IARG_INST_PTR, IARG_END);
+void updateBHRIndex(){
+    int index = 0;
+    for (int i=0; i<BHR_BITS-1; i++) {
+        if (branch_history[i]){
+            index = index | (1<<i);
+        }
+    }
+    bhr_current_index = index;
 }
 
-// This function is called when the application exits
+void setPrediction(){
+    if (bhr[bhr_current_index] > 0){
+        prediction  = true;
+    }
+    else
+    {
+        prediction  = false;
+    }
+}
+
+// update the recent history of branch results
+void updateBranchHistory(bool taken){    
+    for (int i=0; i<BHR_BITS-1; i++) {
+        branch_history[i] = branch_history[i+1];
+    }
+    branch_history[BHR_BITS-1] = taken;
+}
+
+void updateBHT(bool taken){
+    if (taken)
+    {
+        if (bhr[bhr_current_index] == -1) bhr[bhr_current_index]++;
+        bhr[bhr_current_index]++;
+    }
+    else
+    {
+        if (bhr[bhr_current_index] == 1) bhr[bhr_current_index]--;
+        bhr[bhr_current_index]--;
+    }
+    bhr[bhr_current_index] = std::max(bhr[bhr_current_index], -BHT_BITS);
+    bhr[bhr_current_index] = std::min(bhr[bhr_current_index], BHT_BITS);
+}
+
+// Print a branch record
+VOID RecordBranch(VOID * ip, BOOL taken, VOID * addr)
+{
+    fprintf(trace,"%p: %p", ip, addr);
+    num_branches++;
+    updateBHRIndex();
+    setPrediction();
+    
+    if (taken)
+    {
+        fprintf(trace,"\tT");
+        if (prediction) {
+            num_correct++;
+            fprintf(trace," ");
+        }
+        else
+        {
+            fprintf(trace,"*");
+        }
+    }
+    else
+    {
+        fprintf(trace,"\tN");
+        if (!prediction) {
+            num_correct++;
+            fprintf(trace," ");
+        }
+        else
+        {
+            fprintf(trace,"*");
+        }
+    }
+    fprintf(trace," %+d\tcorrect=%d/%d\n", bhr[bhr_current_index], num_correct, num_branches);
+    
+    updateBHT(taken);
+    updateBranchHistory(taken);
+}
+
+// Is called for every instruction and instruments reads and writes
+VOID Instruction(INS ins, VOID *v)
+{
+    if (INS_IsBranch(ins))
+    {
+        INS_InsertCall(
+            ins, IPOINT_BEFORE, (AFUNPTR)RecordBranch,
+            IARG_INST_PTR, IARG_BRANCH_TAKEN, IARG_BRANCH_TARGET_ADDR, 
+            IARG_END);
+    }
+}
+
 VOID Fini(INT32 code, VOID *v)
 {
+    fprintf(trace,"...\nOVERALL correct=%d/%d\t%f using a %d-bit saturating TESTING, %d-bit correlated predictor.\n", 
+        num_correct, num_branches, (double)num_correct/(double)num_branches, BHT_BITS, BHR_BITS);
     fprintf(trace, "#eof\n");
     fclose(trace);
 }
@@ -54,10 +118,10 @@ VOID Fini(INT32 code, VOID *v)
 /* ===================================================================== */
 /* Print Help Message                                                    */
 /* ===================================================================== */
-
+   
 INT32 Usage()
 {
-    PIN_ERROR("This Pintool prints the IPs of every instruction executed\n" 
+    PIN_ERROR( "This Pintool prints a trace of branch instructions\n" 
               + KNOB_BASE::StringKnobSummary() + "\n");
     return -1;
 }
@@ -66,20 +130,27 @@ INT32 Usage()
 /* Main                                                                  */
 /* ===================================================================== */
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-    trace = fopen("itrace.out", "w");
-    
-    // Initialize pin
     if (PIN_Init(argc, argv)) return Usage();
 
-    // Register Instruction to be called to instrument instructions
-    INS_AddInstrumentFunction(Instruction, 0);
-
-    // Register Fini to be called when the application exits
-    PIN_AddFiniFunction(Fini, 0);
+    trace = fopen("branchlog.log", "w");
     
-    // Start the program, never returns
+    // Initialize branch predictor variables
+    for (int i=0; i<PATH_DEPTH; i++) {
+        branch_history[i] = false;
+    }
+    for (int i=0; i<BHR_ENTRIES; i++) {
+        bhr[i] = -1;
+    }
+    bhr_current_index = 0;
+    num_branches = 0;
+    num_correct = 0;
+
+    INS_AddInstrumentFunction(Instruction, 0);
+    PIN_AddFiniFunction(Fini, 0);
+
+    // Never returns
     PIN_StartProgram();
     
     return 0;
