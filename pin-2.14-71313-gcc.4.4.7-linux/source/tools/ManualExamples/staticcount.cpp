151fcb89854139451b8fbd04961a1e3bf3c34348
Qian Wang <qwang2012@gmail.com>
2015-12-03 22:20:16 -0500

    Added 2048 benchmark

--- pin-2.14-71313-gcc.4.4.7-linux/source/tools/ManualExamples/staticcount.cpp
+++ pin-2.14-71313-gcc.4.4.7-linux/source/tools/ManualExamples/staticcount.cpp
@@ -1,79 +1,90 @@
-/*BEGIN_LEGAL 
-Intel Open Source License 
-
-Copyright (c) 2002-2015 Intel Corporation. All rights reserved.
- 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.  Redistributions
-in binary form must reproduce the above copyright notice, this list of
-conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.  Neither the name of
-the Intel Corporation nor the names of its contributors may be used to
-endorse or promote products derived from this software without
-specific prior written permission.
- 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE INTEL OR
-ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-END_LEGAL */
-//
-// This tool prints a trace of image load and unload events
-//
-
 #include <stdio.h>
-#include <iostream>
+#include <math.h>
 #include "pin.H"
 
 
-// Pin calls this function every time a new img is loaded
-// It can instrument the image, but this example merely
-// counts the number of static instructions in the image
+FILE * trace;
 
-VOID ImageLoad(IMG img, VOID *v)
+bool prediction;
+int num_branches;
+int num_correct;
+
+
+void setPrediction(){
+    if (){
+        prediction  = true;
+    }
+    else
+    {
+        prediction  = false;
+    }
+}
+
+// Print a branch record
+VOID RecordBranch(VOID * ip, BOOL taken, VOID * addr)
 {
-    UINT32 count = 0;
+    fprintf(trace,"%p: %p", ip, addr);
+    num_branches++;
     
-    for (SEC sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec))
-    { 
-        for (RTN rtn = SEC_RtnHead(sec); RTN_Valid(rtn); rtn = RTN_Next(rtn))
+    setPrediction();
+    
+    if (taken)
+    {
+        fprintf(trace,"\tT");
+        if (prediction) {
+            num_correct++;
+            fprintf(trace," ");
+        }
+        else
         {
-            // Prepare for processing of RTN, an  RTN is not broken up into BBLs,
-            // it is merely a sequence of INSs 
-            RTN_Open(rtn);
-            
-            for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins))
-            {
-                count++;
-            }
-
-            // to preserve space, release data associated with RTN after we have processed it
-            RTN_Close(rtn);
+            fprintf(trace,"*");
         }
     }
-    fprintf(stderr, "Image %s has  %d instructions\n", IMG_Name(img).c_str(), count);
+    else
+    {
+        fprintf(trace,"\tN");
+        if (!prediction) {
+            num_correct++;
+            fprintf(trace," ");
+        }
+        else
+        {
+            fprintf(trace,"*");
+        }
+    }
+    fprintf(trace," %+d\tcorrect=%d/%d\n", bhr[bhr_current_index], num_correct, num_branches);
+    
+    
+}
+
+// Is called for every instruction and instruments reads and writes
+VOID Instruction(INS ins, VOID *v)
+{
+    if (INS_IsBranch(ins))
+    {
+        INS_InsertCall(
+            ins, IPOINT_BEFORE, (AFUNPTR)RecordBranch,
+            IARG_INST_PTR, IARG_BRANCH_TAKEN, IARG_BRANCH_TARGET_ADDR, 
+            IARG_END);
+    }
+}
+
+VOID Fini(INT32 code, VOID *v)
+{
+    fprintf(trace,"...\nOVERALL correct=%d/%d\t%f using a data-correlated predictor.\n", 
+        num_correct, num_branches, (double)num_correct/(double)num_branches);
+    fprintf(trace, "#eof\n");
+    fclose(trace);
 }
 
 /* ===================================================================== */
 /* Print Help Message                                                    */
 /* ===================================================================== */
-
+   
 INT32 Usage()
 {
-    cerr << "This tool prints a log of image load and unload events" << endl;
-    cerr << " along with static instruction counts for each image." << endl;
-    cerr << endl << KNOB_BASE::StringKnobSummary() << endl;
+    PIN_ERROR( "This Pintool prints a trace of branch instructions\n" 
+              + KNOB_BASE::StringKnobSummary() + "\n");
     return -1;
 }
 
@@ -81,18 +92,21 @@ INT32 Usage()
 /* Main                                                                  */
 /* ===================================================================== */
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-    // prepare for image instrumentation mode
-    PIN_InitSymbols();
-
-    // Initialize pin
     if (PIN_Init(argc, argv)) return Usage();
 
-    // Register ImageLoad to be called when an image is loaded
-    IMG_AddInstrumentFunction(ImageLoad, 0);
+    trace = fopen("branchlog.out", "w");
+    
+    // Initialize branch predictor variables
+    
+    num_branches = 0;
+    num_correct = 0;
 
-    // Start the program, never returns
+    INS_AddInstrumentFunction(Instruction, 0);
+    PIN_AddFiniFunction(Fini, 0);
+
+    // Never returns
     PIN_StartProgram();
     
     return 0;
